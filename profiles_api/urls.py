f11ae2183ca8f9385665730ab9c86f2fafa18bcc
jcurt11n <jcurt11n@gmail.com>
2020-11-23 17:01:05 -0500

    Added login token api - section 11

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
-from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
@@ -11,6 +10,6 @@ router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
-
 ]
